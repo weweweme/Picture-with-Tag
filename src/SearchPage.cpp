18be36d0703c5f243e601ab4ee4e6012c00eeb9d
JinSoo Joo Mac <argentdarae@gmail.com>
2024-04-29 08:48:37 +0900

    feat: Search Page 거ㅁ색 InputField UI 배치

--- src/SearchPage.cpp
+++ src/SearchPage.cpp
@@ -2,8 +2,18 @@
 
 SearchPage::SearchPage(const wxString& title, const wxPoint& pos, const wxSize& size, const PageID currentPage)
         : BasePage(title, pos, size, currentPage) {
+    SearchPage::InitUI();
 }
 
 void SearchPage::InitUI() {
+    BasePage::InitUI();
+
+    // 검색값 입력 필드
+    new wxStaticText(this->panel, wxID_ANY, SEARCH_LABEL_TEXT, wxPoint(SEARCH_LABEL_X, SEARCH_LABEL_Y));
+    this->searchInput = new wxTextCtrl(this->panel, wxID_ANY, "", wxPoint(SEARCH_INPUT_X, SEARCH_INPUT_Y), wxSize(SEARCH_INPUT_FIELD_WIDTH, INPUT_FIELD_HEIGHT));
+    this->searchInput->Bind(wxEVT_TEXT, &SearchPage::OnTitleTextChange, this);
+}
+
+void SearchPage::OnTitleTextChange(wxCommandEvent &_) {
 
 }
--- src/SearchPage.h
+++ src/SearchPage.h
@@ -9,6 +9,19 @@ public:
 
 protected:
     void InitUI() override;
+
+private:
+    void OnTitleTextChange(wxCommandEvent& _);
+
+    wxTextCtrl* searchInput;
+
+    static constexpr char SEARCH_LABEL_TEXT[] = "검색";
+    static constexpr int SEARCH_LABEL_X = 20;
+    static constexpr int SEARCH_LABEL_Y = 25;
+    static constexpr int SEARCH_INPUT_X = 60;
+    static constexpr int SEARCH_INPUT_Y = 20;
+    static constexpr int SEARCH_INPUT_FIELD_WIDTH = 700;
+    static constexpr int INPUT_FIELD_HEIGHT = 25;
 };
 
 #endif //PICTURE_WITH_TAG_SEARCHPAGE_H
